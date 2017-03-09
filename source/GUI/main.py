d77cc0005c0f9e8abc9f8f722a3c76c2dd747026
chet0322 <chet0322@mail.fresnostate.edu>
2017-03-09 13:42:31 -0800

    Redirect GUI files and fix main.py
    -move files to source -> GUI
    -change text view to text edit in main.py

--- source/GUI/main.py
+++ source/GUI/main.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'mainp.ui'
 #
-# Created: Thu Mar 09 12:01:17 2017
+# Created: Thu Mar 09 13:21:40 2017
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -11,30 +11,34 @@ from PySide import QtCore, QtGui
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("Captslog Main")
-        MainWindow.resize(800, 600)
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setfixsize(800, 600)
 		
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 		
-        self.listView = QtGui.QListView(self.centralwidget)
-        self.listView.setGeometry(QtCore.QRect(0, 0, 191, 581))
-        self.listView.setObjectName("listView")
+        self.horizontalLayout = QtGui.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
 		
-        self.textBrowser = QtGui.QTextBrowser(self.centralwidget)
-        self.textBrowser.setGeometry(QtCore.QRect(200, 0, 311, 561))
-        self.textBrowser.setObjectName("textBrowser")
+        self.listView = QtGui.QListView(self.centralwidget)
+        self.listView.setObjectName("listView")
+        self.horizontalLayout.addWidget(self.listView)
+		
+        self.textEdit = QtGui.QTextEdit(self.centralwidget)
+        self.textEdit.setObjectName("textEdit")
+        self.horizontalLayout.addWidget(self.textEdit)
 		
         self.graphicsView = QtGui.QGraphicsView(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(520, 0, 281, 571))
         self.graphicsView.setObjectName("graphicsView")
+        self.horizontalLayout.addWidget(self.graphicsView)
 		
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+		MainWindow.setCentralWidget(self.centralwidget)
+        
+		self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 804, 21))
         self.menubar.setObjectName("menubar")
-		
         MainWindow.setMenuBar(self.menubar)
+		
         self.statusbar = QtGui.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
