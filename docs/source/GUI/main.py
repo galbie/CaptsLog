3de10b1377deda2f9ff71cf47df907ff7637086d
chet0322 <chet0322@mail.fresnostate.edu>
2017-03-09 12:02:36 -0800

    Main.py Update
    added list view, text editor, and text viewer to main

--- docs/source/GUI/main.py
+++ docs/source/GUI/main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'untitled.ui'
+# Form implementation generated from reading ui file 'mainp.ui'
 #
-# Created: Mon Feb 27 12:36:13 2017
+# Created: Thu Mar 09 12:01:17 2017
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -15,6 +15,15 @@ class Ui_MainWindow(object):
         MainWindow.resize(800, 600)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        self.listView = QtGui.QListView(self.centralwidget)
+        self.listView.setGeometry(QtCore.QRect(0, 0, 191, 581))
+        self.listView.setObjectName("listView")
+        self.textBrowser = QtGui.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(200, 0, 311, 561))
+        self.textBrowser.setObjectName("textBrowser")
+        self.graphicsView = QtGui.QGraphicsView(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(520, 0, 281, 571))
+        self.graphicsView.setObjectName("graphicsView")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
