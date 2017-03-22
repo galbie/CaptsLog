422cea25dc1dfa7189a1a4abfe3c7e478bef2153
JeewakaSankalpa <jeewaka@mail.fresnostate.edu>
2017-03-22 07:43:31 -0700

    Added Exceptions to handle ServerTimeout Errors Changed return types to bool

--- captslog/db/DBHandler.py
+++ captslog/db/DBHandler.py
@@ -1,6 +1,6 @@
 import pprint
 from datetime import datetime
-
+import pymongo
 from pymongo import MongoClient
 from bson import ObjectId
 import time
@@ -9,6 +9,8 @@ import time
 class DBHandlerClass:
     client = MongoClient()
     db = client['Captains_Log_DB']
+    print client
+    print db
 
     def __init__(self):
         client = MongoClient()
@@ -27,24 +29,31 @@ class DBHandlerClass:
 
         if name == str(""):
             # print "Error!! name Can't be Empty"
-            return 0
+            return False
         if username == str(""):
             # print "Error!! username Can't be Empty"
-            return 0
+            return False
         if len(username) < 4:
             # print "Error!! username should be at least 4 characters"
-            return 0
+            return False
         if len(user_password) < 5:
             # print "Error!! password should be at least 5 characters"
-            return 0
+            return False
         entry = {"Name": name,
                  "Username": username,
                  "User_Password": user_password,
                  "First_Login_Date": datetime.now(),
                  "Last_Login_Date": datetime.now()}
         t = self.db["User_Table"]
-        if t.insert_one(entry):
-            return 1
+        try:
+            if t.insert_one(entry):
+                return True
+            else:
+                return False
+        except pymongo.errors.ServerSelectionTimeoutError:
+            print 'ERROR : No connection could be made because' \
+                  ' the target machine actively refused it'
+            return True
 
     def insert_to_entries_table(self, title, tags, content):
         """Insert Data into the Entries_Table in the Database
@@ -60,17 +69,22 @@ class DBHandlerClass:
         """
         if title == str(""):
             # print "Error!! Title Can't be Empty"
-            return 0
+            return False
         entry = {"Title": title,
                  "Date_Created": datetime.now(),
                  "Last_Modified": datetime.now(),
                  "Tags": tags,
                  "Content": content}  # TODO reformat the 'content' variable to include the Markup file (data = Binary(open(content).read()))
         t = self.db["Entries_Table"]
-        if t.insert_one(entry):
-            return 1
-
-        return False
+        try:
+            if t.insert_one(entry):
+                return True
+            else:
+                return False
+        except pymongo.errors.ServerSelectionTimeoutError:
+            print 'ERROR : No connection could be made because' \
+                  ' the target machine actively refused it'
+            return True
 
     def search_entries_by_title(self, title):
         """Search For a Specified Title in the Entries_Table
@@ -84,9 +98,15 @@ class DBHandlerClass:
         """
 
         entries_table = self.db["Entries_Table"]
-        result = entries_table.find_one({"Title": title})
-        return result  # TODO Modify to allow multiple results using find(),
-        # TODO also find similar results which are not exact matches
+        try:
+            result = entries_table.find_one({"Title": title})
+            return result
+        except pymongo.errors.ServerSelectionTimeoutError:
+            print 'ERROR : No connection could be made because' \
+                  ' the target machine actively refused it'
+            return False
+            # TODO Modify to allow multiple results using find(),
+            # TODO also find similar results which are not exact matches
 
     def search_entries_by_created_date(self, date):
         """Search For Entries created on the specified Date in the Entries_Table
@@ -101,9 +121,15 @@ class DBHandlerClass:
 
         if date.date() <= datetime.now().date():
             entries_table = self.db["Entries_Table"]
-            return entries_table.find_one({"Date_Created": date})  # TODO Modify to allow multiple results using find()
-
-        return int(0)
+            try:
+                return entries_table.find_one(
+                    {"Date_Created": date})
+                # TODO Modify to allow multiple results using find()
+            except pymongo.errors.ServerSelectionTimeoutError:
+                print 'ERROR : No connection could be made because' \
+                      ' the target machine actively refused it'
+                return True
+        return False
 
     def search_entries_by_modified_date(self, date):
         """Search For Entries modified on the specified Date in the Entries_Table
@@ -117,8 +143,14 @@ class DBHandlerClass:
         """
         if date.date() <= datetime.now().date():
             entries_table = self.db["Entries_Table"]
-            return entries_table.find_one({"Last_Modified": date})  # TODO Modify to allow multiple results using find()
-        return int(0)
+            try:
+                return entries_table.find_one(
+                    {"Last_Modified": date})  # TODO Modify to allow multiple results using find()
+            except pymongo.errors.ServerSelectionTimeoutError:
+                print 'ERROR : No connection could be made because' \
+                      ' the target machine actively refused it'
+                return True
+        return False
 
     def update_entries(self, _id, vals):
         """Update entries in the Entries_Table
@@ -132,12 +164,18 @@ class DBHandlerClass:
         """
 
         entries_table = self.db["Entries_Table"]
-        if not entries_table.find_one({"_id": ObjectId(_id)}):
-            # print "The specified entry does not Exist"
-            return 0
-        vals["Last_Modified"] = datetime.now()
-        entries_table.update_one({"_id": ObjectId(_id)}, {"$set": vals})
-        return 1
+        try:
+            if not entries_table.find_one({"_id": ObjectId(_id)}):
+                # print "The specified entry does not Exist"
+                return False
+            vals["Last_Modified"] = datetime.now()
+            entries_table.update_one({"_id": ObjectId(_id)},
+                                     {"$set": vals})
+            return True
+        except pymongo.errors.ServerSelectionTimeoutError:
+            print 'ERROR : No connection could be made because' \
+                  ' the target machine actively refused it'
+            return True
 
     def delete_entries(self, _id):
         """Delete entries in the Entries_Table
@@ -150,11 +188,16 @@ class DBHandlerClass:
         """
 
         entries_table = self.db["Entries_Table"]
-        if not entries_table.find_one({"_id": ObjectId(_id)}):
-            # print "The specified entry does not Exist"
-            return 0
-        entries_table.delete_one({"_id": ObjectId(_id)})
-        return 1
+        try:
+            if not entries_table.find_one({"_id": ObjectId(_id)}):
+                # print "The specified entry does not Exist"
+                return False
+            entries_table.delete_one({"_id": ObjectId(_id)})
+            return True
+        except pymongo.errors.ServerSelectionTimeoutError:
+            print 'ERROR : No connection could be made because the ' \
+                  'target machine actively refused it'
+            return True
 
     def support_func_get_all(self, lim):
         """This is just a support function which returns all the data in the Entries table upto a specified limit
@@ -164,7 +207,14 @@ class DBHandlerClass:
         Return:
             collection: All the entries in the Entries_table uto the specified limit
         """
-        entries_table = self.db["Entries_Table"]
-        return entries_table.find().limit(lim)
 
-        # TODO Save the Entire text in the MArkdown file instead of the markdown file itself
+        try:
+            entries_table = self.db["Entries_Table"]
+            result = entries_table.find().limit(lim)
+            print result[0]
+            return result
+        except pymongo.errors.ServerSelectionTimeoutError:
+            print 'ERROR : No connection could be made because ' \
+                  'the target machine actively refused it'
+            return False
+            # TODO Save the Entire text in the MArkdown file instead of the markdown file itself
