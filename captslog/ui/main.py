ff19160b7aa1743d9e585ef7e060a1bdfbe08dea
Kasthuri A J S Perera <jeewaka@mail.fresnostate.edu>
2017-05-07 02:42:41 -0700

    new, edit, save buttons coded.

--- captslog/ui/main.py
+++ captslog/ui/main.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, unicode_literals
 
-
 from PySide import QtCore, QtGui
 from captslog.db.DBHandler import DBHandlerClass
 
@@ -27,34 +26,55 @@ except AttributeError:
 # Main Control Class		
 class Main(QtGui.QMainWindow):
     def __init__(self, parent=None):
+        self.current_selection = None
         self.db_handler = DBHandlerClass()
         super(Main, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.center_widget.entry.journalEntry.textChanged.connect(self.text_triggered)
-        self.ui.center_widget.journalList.currentItemChanged.connect(self.itemChanged)
+        self.ui.center_widget.journalList.currentItemChanged.connect(self.item_changed)
+        self.ui.actionNew_Entry.triggered.connect(self.new_entry_action)
+        self.ui.action_Save_Entry.triggered.connect(self.save_button_action)
+        # self.ui.actionNew_Entry.
+        self.refresh_list_view()
+
+    def refresh_list_view(self):
         results = self.db_handler.get_all()
         for x in results:
             item = QtGui.QListWidgetItem('Title: ' + str(x['Title']))
             item.setToolTip(str(x["_id"]))
             self.ui.center_widget.journalList.addItem(item)
+        self.ui.center_widget.entry.journalEntry.setReadOnly(True)
+
+    def new_entry_action(self):
+        self.ui.center_widget.journalList.setEnabled(False)
+        self.current_selection = None
+        self.ui.center_widget.entry.journalEntry.setReadOnly(False)
+        self.ui.center_widget.entry.journalEntry.setPlainText("")
 
     def text_triggered(self):
         raw = self.ui.center_widget.entry.journalEntry.toPlainText()
         md = markdown.Markdown()
-        #raw = raw.encode('utf-8')
+        # raw = raw.encode('utf-8')
         ntxt = md.convert(raw)
         self.ui.center_widget.view.journalView.setHtml(ntxt)
 
-    def itemChanged(self):
+    def save_button_action(self):
+        print(self.db_handler.insert_to_entries_table(self.get_title(), [],
+                                                      self.ui.center_widget.entry.journalEntry.toPlainText()))
+        self.ui.center_widget.entry.journalEntry.setPlainText("")
+        self.refresh_list_view()
+
+    def item_changed(self):
         item = self.ui.center_widget.journalList.currentItem()
         result = self.db_handler.search_entries_by_id(item.toolTip())[0]
-
         st = result["MarkdownFile"].encode('utf-8')
-
-        print(st)
         self.ui.center_widget.entry.journalEntry.setPlainText(st)
         self.text_triggered()
+
+    def get_title(self):
+        return "Sample Title"
+
 if __name__ == "__main__":
     import sys
 
